d64e24505c1222121357821ef033625bf6680763
Lopes-IT <gustavo.lopesv3@gmail.com>
2019-10-27 15:12:39 -0500

    deploy da aplicacao

--- mercale/settings.py
+++ mercale/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
